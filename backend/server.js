87ee0d5ade67ff0d59d6846fa26ab0971825f18f
Suryash Jha <joblesscoder2468@gmail.com>
2025-02-26 16:50:25 +0530

    basic socket connection started

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
-
+const {Server} = require('socket.io')
 const app = express();
 const server = createServer(app);
+const io= new Server(server)
 
 // app.get('/', (req, res) => {
 //   res.send('<h1>Hello world</h1>');
@@ -12,6 +13,10 @@ const server = createServer(app);
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
+
+io.on('connection', (socket)=>{
+    console.log('connected!!!')
+})
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
