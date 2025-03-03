b155bb91bcb9ba8b0af801b4c81f45ffa0ebdac6
Suryash Jha <joblesscoder2468@gmail.com>
2025-03-03 22:26:35 +0530

    socket.io added admin panel

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,28 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const {Server} = require('socket.io')
+const { instrument } = require("@socket.io/admin-ui");
+
 const app = express();
 const server = createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: ['*'],
+//     credentials: true
+
+//   }
+// });
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"]
-  }
+    origin: ["http://localhost:3000", "https://admin.socket.io"], // Allow both frontend & Admin UI
+    credentials: true,
+  },
 });
 
-
+instrument(io, {
+  auth: false,
+  mode: "development",
+});
 
 io.on('connection', (socket)=>{
     // socket.broadcast.emit('messageIn', 'Connected by socket port 3001'+socket.id, )
